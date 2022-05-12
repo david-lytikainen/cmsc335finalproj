14d1b2a461636ff3b436b4d73b27f55a62a169a8
David Lytikainen <77453517+david-lytikainen@users.noreply.github.com>
2022-05-12 17:56:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//put "type": "module" in package.json file and run npm install node-fetch
 import fetch from "node-fetch";
 import http from "http";
 import express from "express";
@@ -6,64 +5,84 @@ import fs from "fs";
 import bodyParser from "body-parser";
 import path from "path";
 import dotenv from "dotenv";
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({path: path.resolve(__dirname, 'credentialsDontPost/.env')});
+dotenv.config({ path: path.resolve(__dirname, "credentialsDontPost/.env") });
 
 let app = express();
 
 
+/* ------------------------------------------------- */
 /* ----Setting up our MongoDB credentials---- */
 const userName = process.env.MONGO_DB_USERNAME;
 const password = process.env.MONGO_DB_PASSWORD;
 const dbName = process.env.MONGO_DB_NAME;
 const collectionName = process.env.MONGO_COLLECTION;
-
-import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri = `mongodb+srv://${userName}:${password}@cluster0.lf3tf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-app.set('views', path.join(__dirname, '/')); //set the views path for res.render()
-app.use(bodyParser.urlencoded({extended:false})); //lets us parse the POST request
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 
-app.get('/',
-function(req,res) {
-  res.writeHead(200, {"Content-Type": "text/html"});
-  res.end(fs.readFileSync("form.ejs"));
+/* ------------------------------------------------- */
+/* ----Setting up the render path and middleware---- */
+app.set("views", path.join(__dirname, "/")); //set the views path for res.render()
+app.use(bodyParser.urlencoded({ extended: false })); //lets us parse the POST request
+
+
+/* ------------------------------------------------- */
+/* ----Returns a list of valid countries---- */
+async function loadCountries() {
+  	const response = await fetch("https://restcountries.com/v2/all");
+  	const result = await response.json();
+  	return result.map((x) => x.name.toLowerCase());
+}
+
+/* ------------------------------------------------- */
+/* ----Rendering the url endpoints---- */
+app.get("/", function (req, res) {
+  	res.writeHead(200, { "Content-Type": "text/html" });
+  	res.end(fs.readFileSync("form.ejs"));
 });
 
-app.post('/processRequest',
-function(req,res) {
-  let { firstName, lastName, country, clientCountry } = req.body;
+app.post("/processRequest", function (req, res) {
+  	let { firstName, lastName, country, clientCountry } = req.body;
 
-  (async() => {
-    try {
-        await client.connect();
+  	async function conn() {
 
-        let client = {firstName: firstName, lastName:lastName, country:country, clientCountry:clientCountry};
-        await client.db(dbName).collection(collectionName).insertOne(client);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-  })();
+		const uri = `mongodb+srv://${userName}:${password}@cluster0.lf3tf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+		const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
+		try {
+	  		await client.connect();
+			let user = {firstName: firstName,lastName: lastName,country: country,clientCountry: clientCountry,};
 
-  let fe = fetch('https://restcountries.com/v2/name/' + country);
-  let result = fe.json();
+		  	await client.db(dbName).collection(collectionName).insertOne(user);
+		} catch (e) {
+		  	console.error(e);
+		} finally {
+		  	await client.close();
+		}
+  	}
 
-  res.render("processRequests.ejs", 
-            {country:result.name, region:result.region, independent:result.independent, flag:result.flag});
+  	async function loadPage() {
 
+		loadCountries().then(async (ret) => {
+			if(!ret.includes(country.toLowerCase())) { //handles if they input an incorrect country
+				res.render("processRequest.ejs", {country: country,region: "You kinda look like this guy ^",
+					independent: "Have a nice day and learn how to spell :)",
+					flag: "https://wallsdesk.com/wp-content/uploads/2017/01/Monkey-full-HD.jpg"
+				});
+			} else { //if they input a valid country
+				const response = await fetch(`https://restcountries.com/v2/name/${country}`);
+				const resul = await response.json();
+				const result = resul[0]; //gets only the first json which is our country
+				res.render("processRequest.ejs", {country: result.name,region: result.region,independent: result.independent,flag: result.flag});
+			}
+		});
+	}
+
+	conn(); //connect to our DB
+	loadPage(); //render the page
 });
-
-/*fetch('https://restcountries.com/v2/name/china')
-  .then(res => res.json())
-  .then(data => console.log(data[0])); */
-
 
 http.createServer(app).listen(3000);
 process.stdout.write("Server started and running on http://localhost:3000\nType Control+C to stop.");
